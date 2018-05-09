94a5aa21001dcf8051a5f8c4df98b9a8fcdfd3e3
fassler <fasslero@gmail.com>
2018-05-09 18:48:29 +0300

    WIP

--- bp.c
+++ bp.c
@@ -3,7 +3,7 @@
 
 #include "bp_api.h"
 
-typedef enum  {WNT, NT, WT, T} Prediction;
+typedef enum  {SNT=0, WNT, WT, ST} Prediction;
 
 typedef struct  {
 
@@ -53,20 +53,38 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
 		MyBP.BTB[i].target = 0;
 	}
 
+	//init sim stat
+	MyBP.stats.br_num = 0;
+	MyBP.stats.flush_num = 0;
+	MyBP.stats.size = 0; //TODO calc
+
 	return 0;
 
 }
 
+
 bool BP_predict(uint32_t pc, uint32_t *dst){
+	TableLine btbLine;
+	uint32_t defaultDstAddress = pc + 4;
+
+	//get the BTB line
+
+	//check whether the line is the desired one, if not set dst to default dst
+
+	//if the address is there, check the Bimodal and return dst according
+
 	return false;
 }
 
+
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
+
 	return;
 }
 
+
 void BP_GetStats(SIM_stats *curStats) {
-	return;
+	return *MyBP.stats;
 }
 
 
